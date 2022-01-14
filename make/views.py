21545a488468849e1d528592ce10d2977aa071bb
wangbin958581347 <34332098+wangbin958581347@users.noreply.github.com>
2022-01-14 13:54:54 +0800

    注释存库代码

--- make/views.py
+++ make/views.py
@@ -13,8 +13,6 @@ import json
 import pymysql
 
 
-
-
 def insert_and_get_id(sql):
     """
     插入并获取其自增 ID：支持批量插入多条
@@ -36,7 +34,9 @@ def insert_and_get_id(sql):
     return insert_id
 
 
-headers = {'Authorization':'Bearer 5_crM9D0ZQEjTmJm6P_J9CjAgxU06AKt0ZB-xeAb'}
+headers = {'Authorization': 'Bearer 5_crM9D0ZQEjTmJm6P_J9CjAgxU06AKt0ZB-xeAb'}
+
+
 def create_nft(request):
     try:
         if request.method == 'POST':
@@ -45,10 +45,10 @@ def create_nft(request):
             layers = params.get('layers')
             group_id = params.get('group_id')
             name = params.get('name')
-            occupation = params.get('occupation')
-            custom_name = params.get('custom_name')
-            original = params.get('original')
-            sex = params.get('sex')
+            # occupation = params.get('occupation')
+            # custom_name = params.get('custom_name')
+            # original = params.get('original')
+            # sex = params.get('sex')
 
             if layers:
                 lenth = len(layers)
@@ -56,25 +56,25 @@ def create_nft(request):
                 layer = Image.open('./files/base/background/white.png').convert('RGBA')
                 final = Image.new("RGBA", layer.size)
                 score = 0
-                composite_card = {
-                    'occupation':occupation,
-                    'custom_name':custom_name,
-                    'state':1,
-                    'original':original,
-                    'sex':sex,
-                                  }
-                element_dict = {
-                    'Background':'background_id',
-                    'Clothes': 'clothes_id',
-                    'Facial Expression': 'expression_id',
-                    'Head': 'head_id',
-                }
+                # composite_card = {
+                #    'occupation':occupation,
+                #    'custom_name':custom_name,
+                #    'state':1,
+                #    'original':original,
+                #    'sex':sex,
+                #                  }
+                # element_dict = {
+                #    'Background':'background_id',
+                #    'Clothes': 'clothes_id',
+                #    'Facial Expression': 'expression_id',
+                #    'Head': 'head_id',
+                # }
                 for x in range(lenth):
                     x = str(x)
                     element_name = layers[x]['property']
                     png_name = layers[x]['name']
                     score += layers[x]['score']
-                    composite_card[element_dict[element_name]] = layers[x]['nft_id']
+                    # composite_card[element_dict[element_name]] = layers[x]['nft_id']
                     layer = Image.open(f'./files/{group_id}/{element_name}/{png_name}.png').convert('RGBA')
                     final = Image.alpha_composite(final, layer)
 
@@ -86,63 +86,58 @@ def create_nft(request):
                     "https://api.cloudflare.com/client/v4/accounts/06d48301c855502bf143d5a4c5d3a982/images/v1",
                     files=files,
                     headers=headers
-                    )
+                )
                 image_link = response.json()['result']['variants'][0]
                 info_value = {
                     'nft_id': 0,
                     'group_id': group_id,
-                    'type':'',
+                    'type': '',
                     'name': name,
                     'owner': '',
-                    'image_link':image_link,
+                    'image_link': image_link,
                     'image_data': '',
                     'score': score,
                     'rank': 0,
                     'created': 0,
-                    'state':0,
+                    'state': 0,
                     'create_time': int(time.time() * 1000),
                     'update_time': int(time.time() * 1000)
                 }
                 # 上传nft_info表
-                sql = f"""
-                insert into 
-                nft_info(`nft_id`,`group_id`,`type`,`name`,`owner`,`image_link`,`image_data`,`score`,`rank`,`created`,`state`,`create_time`,`update_time`)
-                values({info_value['nft_id']},{info_value['group_id']},"{info_value['type']}","{info_value['name']}","{info_value['owner']}","{info_value['image_link']}","{info_value['image_data']}",{info_value['score']},{info_value['rank']},{info_value['created']},{info_value['state']},{info_value['create_time']},{info_value['update_time']})
-                """
-
-                info_id = insert_and_get_id(sql)
+                # sql = f"""
+                # insert into
+                # nft_info(`nft_id`,`group_id`,`type`,`name`,`owner`,`image_link`,`image_data`,`score`,`rank`,`created`,`state`,`create_time`,`update_time`)
+                # values({info_value['nft_id']},{info_value['group_id']},"{info_value['type']}","{info_value['name']}","{info_value['owner']}","{info_value['image_link']}","{info_value['image_data']}",{info_value['score']},{info_value['rank']},{info_value['created']},{info_value['state']},{info_value['create_time']},{info_value['update_time']})
+                # """
+                #
+                # info_id = insert_and_get_id(sql)
                 # 上传nft_composite_card表
-                composite_card['info_id'] = info_id
-                columns_str = ''
-                value_str = ''
-                for k,v in composite_card.items():
-                    columns_str += '`' + k + '`,'
-                    if k in ['occupation','custom_name']:
-                        value_str += '"' + str(v) + '",'
-                    else:
-                        value_str += str(v) + ','
-
-                sql = f"""
-                insert into 
-                nft_composite_card({columns_str[:-1]})
-                values({value_str[:-1]})
-                """
-                insert_and_get_id(sql)
-                result = {"status": "success", "message": "上传成功","url":image_link}
+                # composite_card['info_id'] = info_id
+                # columns_str = ''
+                # value_str = ''
+                # for k,v in composite_card.items():
+                #    columns_str += '`' + k + '`,'
+                #    if k in ['occupation','custom_name']:
+                #        value_str += '"' + str(v) + '",'
+                #    else:
+                #        value_str += str(v) + ','
+                #
+                # sql = f"""
+                # insert into
+                # nft_composite_card({columns_str[:-1]})
+                # values({value_str[:-1]})
+                # """
+                # insert_and_get_id(sql)
+                result = {"status": "success", "message": "上传成功", "url": image_link}
                 return HttpResponse(json.dumps(result))
             else:
-                result = {"status": "faild", "message": "图层数据为空","url":""}
+                result = {"status": "faild", "message": "图层数据为空", "url": ""}
                 return HttpResponse(json.dumps(result))
 
         else:
-            result = {"status":"faild","message":"方法错误（POST）","url":""}
+            result = {"status": "faild", "message": "方法错误（POST）", "url": ""}
             return HttpResponse(json.dumps(result))
     except:
         print(traceback.format_exc())
-        result = {"status": "faild", "message": "error","url":""}
+        result = {"status": "faild", "message": "error", "url": ""}
         return HttpResponse(json.dumps(result))
-
-
-
-
-
